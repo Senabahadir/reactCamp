62d1cc1e3c2ddcb99e94516fc14dd694af3be782
Sena <senabahadiroglu0@gmail.com>
2022-07-27 18:12:04 +0300

    Router eklendi.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  // <React.StrictMode>
-    <App />, document.getElementById('root')    //App componentini roota yerleştir demek. Yani uygulamamızın görüntüsü App olacak. App'nin içinde de başka componentler kullanarak oradaki hiyerarşiyi kuruyo olacağız.
-  // </React.StrictMode>
-);
+ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root'));
+
+reportWebVitals();
+// ReactDOM.render(
+//   <Router>
+//     <Link to="/"> App</Link>
+ 
+//   <Routes>
+//     <Route path="/" element={<App/>} />
+//   </Routes>
+//   </Router>,
+//   document.getElementById("root")
+// )
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import ProductList from "../pages/ProductList";
 import Categories from "./Categories"
-import { Grid, GridColumn, GridRow } from 'semantic-ui-react'
+import { Grid } from 'semantic-ui-react'
+import { Route, Routes } from 'react-router-dom'
+import ProductDetail from '../pages/ProductDetail';
 
 
 export default function Dashboard() {
     return (
         <div>
             <Grid>
-                <GridRow>
-                    <GridColumn width={4}>
+                <Grid.Row>
+                    <Grid.Column width={4}>
                         <Categories></Categories>
-                    </GridColumn>
-                    <GridColumn width={12}>
-                        <ProductList></ProductList>
-                    </GridColumn>
-                </GridRow>
+                    </Grid.Column>
+                    <Grid.Column width={12}>
+                        <Routes>
+                            <Route exact path="/" element={<ProductList />} />
+                            <Route exact path="/products" element={<ProductList />} />
+                            <Route path="/products/:id" element={<ProductDetail/>} />
+                        </Routes>
+                    </Grid.Column>
+                </Grid.Row>
             </Grid>
 
 
